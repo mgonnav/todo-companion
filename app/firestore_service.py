c2ddfda6eb3e184f3412bbd8bf9ed3ce7576ef48
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-08-15 23:40:58 -0500

    Firebase get app or create it if not found

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -2,7 +2,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 credential = credentials.ApplicationDefault()
-firebase_admin.initialize_app(credential)
+project_id = 'mgonnav-flask-todo'
+
+try:
+    firebase_admin.get_app()
+except ValueError:
+    firebase_admin.initialize_app(credential, {'projectId': project_id})
 
 db = firestore.client()
 
